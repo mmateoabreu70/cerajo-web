759df1497839e0c41bddecd3f58ff2c78ddefc58
Michael David Mateo Abreu <mmateoabreu70@gmail.com>
2024-11-25 00:36:51 -0400

    Layout separation
    Applying CSS Grid to separate page sections

--- client-app/app/page.js
+++ client-app/app/page.js
@@ -1,26 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      {/* <div class="one">One</div>
-      <div class="two">Two</div>
-      <div class="three">Three</div>
-      <div class="four">Four</div>
-      <div class="five">Five</div>
-      <div class="one">Six</div>
-      <div class="two">Seven</div>
-      <div class="three">Eight</div>
-      <div class="four">Nine</div>
-      <div class="five">Ten</div>
-      <div class="one">One</div>
-      <div class="two">Two</div>
-      <div class="three">Three</div>
-      <div class="four">Four</div>
-      <div class="five">Five</div>
-      <div class="one">Six</div>
-      <div class="two">Seven</div>
-      <div class="three">Eight</div>
-      <div class="four">Nine</div>
-      <div class="five">Ten</div> */}
+    <main className="content">
+      
     </main>
   );
 }
